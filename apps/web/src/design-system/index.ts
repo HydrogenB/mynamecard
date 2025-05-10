992c606554cd774ef15e33c9a1fb92b460b4e71c
HydrogenB <bird_create@hotmail.com>
2025-05-10 14:58:56 +0700

    Add foundational design system files for colors, spacing, and typography

---
+++ apps/web/src/design-system/index.ts
@@ -0,0 +1,17 @@
+// Foundation exports
+export * from './foundations/colors';
+export * from './foundations/typography';
+export * from './foundations/spacing';
+
+// Component exports
+export * from './components/Button';
+export * from './components/Card';
+export * from './components/Input';
+export * from './components/Text';
+export * from './components/Spinner';
+export * from './components/QRCode';
+export * from './components/Container';
+export * from './components/Badge';
+export * from './components/Divider';
+export * from './components/IconButton';
+export * from './components/LanguageSwitcher';
